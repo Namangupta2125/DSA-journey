328f33d337799fb5b3cf39d4dbe22b00054aed61
Namangupta2125 <116955157+Namangupta2125@users.noreply.github.com>
2023-11-20 19:22:59 +0530

    Add files via upload

---
+++ list(doubly linked list stl function).cpp
@@ -0,0 +1,58 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+
+
+// int main()
+// {
+// 	list<int>l={1,2,3,4,5,3,6};
+// 	// l.remove(element) - remove all the occurence of given element from the list
+//  /* l.erase(iterator) - removes the element at the given iterator
+//     iterator can't be given like l.begin()+n - where n is any number but one because the memory is not continious
+// 	we can't increase the iterator we have to always increment iterator by 1 by doing increment operation
+// 	t++;
+
+// 	example
+// 	it = l.begin();
+// 	cout<<*it;
+// 	output = 1
+// 	it++;
+// 	cout<<*it;
+// 	output = 2
+// 	it++;
+
+//  */
+//  // l.insert(iterator,element) - the elment is inserted at the given position 
+//  //l.push_back();
+//  //l.push_front();
+//  //l.pop_back();
+//  //l.pop_front();
+//  // all the insertion and deletion operation are bieng done in O(1) time 
+
+    
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+
+
+// int main()
+// {
+// 	list<int>l={1,2,3,4,5,3,6,6,6,6,6,6};
+// 	// l.remove(6);
+// 	// auto it = l.begin();
+// 	// it++;
+// 	// it++;
+// 	// cout<<*it<<endl;
+// 	// auto it = l.begin();
+// 	// it++;
+// 	// it++;
+// 	// l.erase(it);
+// 	auto it = l.begin();
+// 	it++;
+// 	it++;
+// 	l.insert(it,26);
+// 	for(auto x:l)
+// 	cout<<x<<" ";
+// }
